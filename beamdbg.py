1fc967a9b9cee5627dc0a2d75800de1fe333c971
jw2202 <jinwoopark2202@gmail.com>
2024-02-18 00:42:53 +0900

    added detecting unreadable chars

--- beamdbg.py
+++ beamdbg.py
@@ -1,6 +1,8 @@
 import sys
 import logmaster as log
 
+version = "0.0.0"
+log.printInfo("Beamdbg v" + version)
 if len(sys.argv) != 4:
     log.printError(f"Usage: {sys.argv[0]} <code file> <input file> <output file>", 1)
 
@@ -9,19 +11,26 @@ input_file = sys.argv[2]
 output_file = sys.argv[3]
 
 try:
-    log.printInfo(f"Loading code file \"{code_file}\"...")
+    log.printInfo(f"Loading code file \033[34m{code_file}\033[0m...")
     with open(code_file, "r") as f:
         code = f.read()
-    log.printInfo(f"Code file successfully loaded \"{code_file}\"")
+    log.printInfo(f"Code file successfully loaded \033[34m{code_file}\033[0m")
 except FileNotFoundError:
-    log.printError(f"Code file not found \"{code_file}\"", 2)
+    log.printError(f"Code file not found \033[34m{code_file}\033[0m", 2)
 
 try:
-    log.printInfo(f"reading input file \"{input_file}\"...")
+    log.printInfo(f"Loading input file \033[32m{input_file}\033[0m...")
     with open(input_file, "r") as f:
         inp = f.read()
-    log.printInfo(f"successfully readed input file \"{input_file}\"")
+    log.printInfo(f"Input file successfully loaded \033[32m{input_file}\033[0m")
 except FileNotFoundError:
-    log.printError(f"Cannot found input file \"{input_file}\"", 3)
+    log.printError(f"Cannot found input file \033[32m{input_file}\033[0m", 3)
 
 output_fp = open(output_file, "w")
+
+print()
+log.printInfo("Starting to debug...")
+readable_chars = "><^v+-@:/\\!?|_HSLsgPpun`')(r\n "
+for c in code:
+    if c not in readable_chars:
+        log.printWarning(f"Found unreadable char \033[31m{c}\033[0m in code, it will be ignored.")
--- logmaster.py
+++ logmaster.py
@@ -1,7 +1,7 @@
-def printInfo(msg):
+def printInfo(msg: str):
     print("[\033[34m*\033[0m] " + msg)
-def printWarning(msg):
+def printWarning(msg: str):
     print("[\033[33m!\033[0m] " + msg)
-def printError(msg, exitcode):
+def printError(msg: str, exitcode: int):
     print("[\033[31mX\033[0m] " + msg)
     exit(exitcode)
