a428586af5e5b470bd2902d80dfdb7b11bb18a65
h0pler <h0pler@proton.me>
2024-02-22 18:07:09 +0900

    advanced 'ni' and file i/o

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import logmaster as log
 import help
 
-version = "0.0.0"
+version = "0.0.1"
 log.printInfo("Beamdbg v" + version)
 if len(sys.argv) != 4:
     log.printError(f"Usage: {sys.argv[0]} <code file> <input file> <output file>", 1)
@@ -11,61 +11,27 @@ code_file = sys.argv[1]
 input_file = sys.argv[2]
 output_file = sys.argv[3]
 
-try:
-    log.printInfo(f"Loading code file \033[34m{code_file}\033[0m...")
-    with open(code_file, "r") as f:
-        code = f.read()
-    log.printInfo(f"Code file successfully loaded \033[34m{code_file}\033[0m")
-except FileNotFoundError:
-    log.printError(f"Code file not found \033[34m{code_file}\033[0m", 2)
-
-try:
-    log.printInfo(f"Loading input file \033[32m{input_file}\033[0m...")
-    with open(input_file, "r") as f:
-        inp = f.read()
-    log.printInfo(f"Input file successfully loaded \033[32m{input_file}\033[0m")
-except FileNotFoundError:
-    log.printError(f"Cannot found input file \033[32m{input_file}\033[0m", 3)
-
 output_fp = open(output_file, "w")
 
-print()
-log.printInfo("Starting to debug...")
-readable_chars = "><^v+-@:/\\!?|_HSLsgPpun`')(r\n "
-for c in code:
-    if c not in readable_chars:
-        log.printWarning(f"Found unreadable char \033[31m{c}\033[0m in code, it will be ignored.")
 
-prefix = "beamdbg> "
-splitted_code = code.split("\n")
-height = len(splitted_code)
-width = max([len(l) for l in splitted_code])
-
-def show_code(x = None, y = None):
+def show_code(x=None, y=None):
     if x == None and y == None:
         print(code)
     else:
         print(code[y][x])
         return code[y][x]
 
-log.printInfo(f"Code height: {height}")
-log.printInfo(f"Code width: {width}")
-log.printInfo("For help, type \"help\".")
 
 RIGHT = 0
 LEFT = 1
 UP = 2
 DOWN = 3
+
+
 class Process:
     def __init__(self, code, input, height, width):
         self.index_x = 0
         self.index_y = 0
-        # self.current_direction = {
-        #     ">": "R",
-        #     "<": "L",
-        #     "^": "U",
-        #     "v": "D"
-        # }
         self.current_direction = RIGHT
         self.beam = 0
         self.store = 0
@@ -77,6 +43,8 @@ class Process:
         self.width = width
         self.halted = False
         self.reason = ""
+        self.output = ""
+
     def interpret(self, c):
         if self.halted:
             return self.reason
@@ -93,40 +61,67 @@ class Process:
         elif c == "-":
             self.beam -= 1
         elif c == "@":
-            sys.stdout.write(chr(self.beam))
+            # sys.stdout.write(chr(self.beam))
+            print("Received output :", chr(self.beam))
+            output_fp.write(chr(self.beam))
+            self.output += chr(self.beam)
         elif c == ":":
-            sys.stdout.write(str(self.beam))
+            # sys.stdout.write(str(self.beam))
+            print("Received output :", str(self.beam))
+            output_fp.write(str(self.beam))
+            self.output += str(self.beam)
         elif c == "/":
-            if self.current_direction == RIGHT: self.current_direction = UP
-            elif self.current_direction == LEFT: self.current_direction = DOWN
-            elif self.current_direction == UP: self.current_direction = RIGHT
-            elif self.current_direction == DOWN: self.current_direction = LEFT
+            if self.current_direction == RIGHT:
+                self.current_direction = UP
+            elif self.current_direction == LEFT:
+                self.current_direction = DOWN
+            elif self.current_direction == UP:
+                self.current_direction = RIGHT
+            elif self.current_direction == DOWN:
+                self.current_direction = LEFT
         elif c == "\\":
-            if self.current_direction == RIGHT: self.current_direction = DOWN
-            elif self.current_direction == LEFT: self.current_direction = UP
-            elif self.current_direction == UP: self.current_direction = LEFT
-            elif self.current_direction == DOWN: self.current_direction = RIGHT
+            if self.current_direction == RIGHT:
+                self.current_direction = DOWN
+            elif self.current_direction == LEFT:
+                self.current_direction = UP
+            elif self.current_direction == UP:
+                self.current_direction = LEFT
+            elif self.current_direction == DOWN:
+                self.current_direction = RIGHT
         elif c == "!":
             if self.beam != 0:
-                if self.current_direction == RIGHT: self.current_direction = LEFT
-                elif self.current_direction == LEFT: self.current_direction = RIGHT
-                elif self.current_direction == UP: self.current_direction = DOWN
-                elif self.current_direction == DOWN: self.current_direction = UP
+                if self.current_direction == RIGHT:
+                    self.current_direction = LEFT
+                elif self.current_direction == LEFT:
+                    self.current_direction = RIGHT
+                elif self.current_direction == UP:
+                    self.current_direction = DOWN
+                elif self.current_direction == DOWN:
+                    self.current_direction = UP
         elif c == "?":
             if self.beam == 0:
-                if self.current_direction == RIGHT: self.current_direction = LEFT
-                elif self.current_direction == LEFT: self.current_direction = RIGHT
-                elif self.current_direction == UP: self.current_direction = DOWN
-                elif self.current_direction == DOWN: self.current_direction = UP
+                if self.current_direction == RIGHT:
+                    self.current_direction = LEFT
+                elif self.current_direction == LEFT:
+                    self.current_direction = RIGHT
+                elif self.current_direction == UP:
+                    self.current_direction = DOWN
+                elif self.current_direction == DOWN:
+                    self.current_direction = UP
         elif c == "|":
-            if self.current_direction == RIGHT: self.current_direction = LEFT
-            elif self.current_direction == LEFT: self.current_direction = RIGHT
+            if self.current_direction == RIGHT:
+                self.current_direction = LEFT
+            elif self.current_direction == LEFT:
+                self.current_direction = RIGHT
         elif c == "_":
-            if self.current_direction == UP: self.current_direction = DOWN
-            elif self.current_direction == DOWN: self.current_direction = UP
+            if self.current_direction == UP:
+                self.current_direction = DOWN
+            elif self.current_direction == DOWN:
+                self.current_direction = UP
         elif c == "H":
             self.halted = True
             self.reason = "OK"
+            print("Output :", self.output)
         elif c == "S":
             self.store = self.beam
         elif c == "L":
@@ -140,46 +135,60 @@ class Process:
         elif c == "p":
             self.beam = self.memory[self.store]
         elif c == "u":
-            if self.beam != self.store: self.current_direction = UP
+            if self.beam != self.store:
+                self.current_direction = UP
         elif c == "n":
-            if self.beam != self.store: self.current_direction = DOWN
+            if self.beam != self.store:
+                self.current_direction = DOWN
         elif c == "`":
             self.store -= 1
         elif c == "'":
             self.store += 1
         elif c == ")":
-            if self.store != 0: self.current_direction = LEFT
+            if self.store != 0:
+                self.current_direction = LEFT
         elif c == "(":
-            if self.store != 0: self.current_direction = RIGHT
+            if self.store != 0:
+                self.current_direction = RIGHT
         elif c == "r":
             self.beam = self.input[self.input_idx]
             self.input_idx += 1
-    def ni(self):
-        if self.halted:
-            return self.reason
-        print(self.index_x, self.index_y)
-        self.interpret(self.code[self.index_y][self.index_x])
-        if self.current_direction == RIGHT:
-            self.index_x += 1
-            if self.index_x >= self.width:
-                self.halted = True
-                self.reason = "Beam was out of range"
-        elif self.current_direction == LEFT:
-            self.index_x -= 1
-            if self.index_x < 0:
-                self.halted = True
-                self.reason = "Beam was out of range"
-        elif self.current_direction == UP:
-            self.index_y -= 1
-            if self.index_y < 0:
-                self.halted = True
-                self.reason = "Beam was out of range"
-        elif self.current_direction == DOWN:
-            self.index_y += 1
-            if self.index_y >= self.height:
-                self.halted = True
-                self.reason = "Beam was out of range"
-        
+
+    def ni(self, count):
+        for i in range(count):
+            if self.halted:
+                return self.reason
+            print(self.index_x, self.index_y)
+            self.interpret(self.code[self.index_y][self.index_x])
+            if self.current_direction == RIGHT:
+                self.index_x += 1
+                if self.index_x >= self.width:
+                    self.halted = True
+                    print("Output :", self.output)
+                    self.reason = "Beam was out of range"
+            elif self.current_direction == LEFT:
+                self.index_x -= 1
+                if self.index_x < 0:
+                    self.halted = True
+                    print("Output :", self.output)
+                    self.reason = "Beam was out of range"
+            elif self.current_direction == UP:
+                self.index_y -= 1
+                if self.index_y < 0:
+                    self.halted = True
+                    print("Output :", self.output)
+                    self.reason = "Beam was out of range"
+            elif self.current_direction == DOWN:
+                self.index_y += 1
+                if self.index_y >= self.height:
+                    self.halted = True
+                    print("Output :", self.output)
+                    self.reason = "Beam was out of range"
+
+
+prefix = "beamdbg> "
+log.printInfo('For help, type "help".')
+p = None
 
 while True:
     cmd = input(prefix)
@@ -190,16 +199,60 @@ while True:
     cmd = cmd.split(" ")
     if len(cmd) > 1:
         param = cmd[1:]
+    else:
+        param = None
     cmd = cmd[0]
     if cmd == "help":
         print(help.help_msg)
     elif cmd == "b":
         continue
     elif cmd == "ni":
-        p.ni()
+        if not p:
+            log.printWarning("Process not found")
+            continue
+        if not param:
+            p.ni(1)
+        else:
+            try:
+                p.ni(int(param[0]))
+            except ValueError:
+                log.printWarning("ValueError")
+                log.printWarning("Usage: ni <int>")
+                continue
     elif cmd == "start":
-        p=Process(splitted_code, inp, height, width)
+        print()
+        log.printInfo("Starting to debug...")
+        readable_chars = "><^v+-@:/\\!?|_HSLsgPpun`')(r\n "
+        try:
+            log.printInfo(f"Loading code file \033[34m{code_file}\033[0m...")
+            with open(code_file, "r") as f:
+                code = f.read()
+            log.printInfo(f"Code file successfully loaded \033[34m{code_file}\033[0m")
+        except FileNotFoundError:
+            log.printError(f"Code file not found \033[34m{code_file}\033[0m", 2)
+
+        try:
+            log.printInfo(f"Loading input file \033[32m{input_file}\033[0m...")
+            with open(input_file, "r") as f:
+                inp = f.read()
+            log.printInfo(f"Input file successfully loaded \033[32m{input_file}\033[0m")
+        except FileNotFoundError:
+            log.printError(f"Cannot found input file \033[32m{input_file}\033[0m", 3)
+            for c in code:
+                if c not in readable_chars:
+                    log.printWarning(
+                        f"Found unreadable char \033[31m{c}\033[0m in code, it will be ignored."
+                    )
+        splitted_code = code.split("\n")
+        height = len(splitted_code)
+        width = max([len(l) for l in splitted_code])
+        log.printInfo(f"Code height: {height}")
+        log.printInfo(f"Code width: {width}")
+        p = Process(splitted_code, inp, height, width)
+        print()
+    elif cmd == "run":
+        continue
     elif cmd == "exit" or cmd == "quit":
         exit(0)
     else:
-        log.printInfo("Unknown command. type \"help\" for help.")
+        log.printInfo('Unknown command. type "help" for help.')
