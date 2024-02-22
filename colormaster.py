7d18f33ec30eba2634b7d97ec16fcddb46dd65de
h0pler <h0pler@proton.me>
2024-02-22 19:08:04 +0900

    colormaster

---
+++ colormaster.py
@@ -0,0 +1,23 @@
+def set_color(color,isBright,isBackground):
+  colors = {
+    "BLACK": 0,
+    "RED": 1,
+    "GREEN": 2,
+    "YELLOW": 3,
+    "BLUE": 4,
+    "MAGENTA": 5,
+    "CYAN": 6,
+    "WHITE": 7
+  }  
+  basecode=30
+  magic="\033[{}m"
+  backgroundcode=10
+  brightcode=60
+  result=0
+  result=colors.get(color.upper(), None)+basecode
+  if isBackground:
+    result+=backgroundcode
+  if isBright:
+    result+=brightcode
+  result=magic.format(result)
+  return result
